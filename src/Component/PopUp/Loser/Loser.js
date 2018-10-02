7e51d9af8c88fc72a3851e5332f48f91e7c5d97b
Kelby Blakeslee <kelby.blakeslee@yahoo.com>
2018-10-02 17:49:19 -0600

    Worked on PopUp (Winner & Loser)

---
+++ src/Component/PopUp/Loser/Loser.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loser() {
+    
+    return (
+        <div>
+            <h1>Give up loser!</h1>
+        </div>
+    )
+}
---
+++ src/Component/PopUp/Winner/Winner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Winner() {
+    
+    return (
+        <div>
+            <h1>You've made it to the Moon!</h1>
+        </div>
+    )
+}
