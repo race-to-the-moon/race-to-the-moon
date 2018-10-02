778272f2cca2c196bd1f4704aa4302ea2ae4ec98
Isaiah Olson <isaiahols@gmail.com>
2018-10-02 09:59:00 -0600

    add .env file and add to gitignore

---
+++ src/ducks/initialState.js
@@ -0,0 +1,22 @@
+const initialState = {
+    rocket: {
+        health: int ?,
+        hit: false,
+        time: int(seconds),
+        alive: true,
+        boost: false,
+        location: ?,
+        invincible: false ?
+   },
+    asteroid: [
+        {
+            location: ?,
+            hit: false,
+            hitByLaser: false
+        }
+    ],
+    laser: {
+        location: 
+  }
+
+}
