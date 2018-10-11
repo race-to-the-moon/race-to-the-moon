09dfca0047d040959e3242a69cb3cc4423765d61
Kelby Blakeslee <kelby.blakeslee@yahoo.com>
2018-10-10 20:55:08 -0600

    added CSS to single mode but had issues with sizing outside of mobile

--- src/Component/HUD/Score/Score.js
+++ src/Component/HUD/Score/Score.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import './Score.css'
 
 function Score(props) {
 
     return (
-        <div>
+        <div className="score-container">
+            <link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet"></link>
             {props.score.astScore}
         </div>
     )
 }
 
 function mapStateToProps(state) {
-    const {score} = state
+    const { score } = state
     return {
         score
     }
